992135aa78d2880035ee433c8e712e3637289311
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-05-28 11:18:59 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import defaultDataset from "./dataset";
 import './assets/styles/style.css';
 import {AnswersList, Chats} from './components/index';
 import FormDialog from './components/forms/FormDialog';
+import {db} from './firebase/index';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -11,7 +11,7 @@ export default class App extends React.Component {
       answers: [],
       chats: [],
       currentId: "init",
-      dataset: defaultDataset,
+      dataset: {},
       open: false
     }
 
@@ -80,9 +80,25 @@ export default class App extends React.Component {
     this.setState({ open: false }); // Once handleClose() works, open state will be switched to false. The modal will close.
   };
 
+  initDataset = (dataset) => {
+    this.setState({dataset: dataset});
+  }
+
   componentDidMount() {
-    const initAnswer = ""; // Initialize the answer
-    this.selectAnswer(initAnswer, this.state.currentId);
+    (async() => {
+      const dataset = this.state.dataset;
+      await db.collection('questions').get().then(snapshots => {
+        snapshots.forEach(doc => {
+          const id = doc.id;
+          const data = doc.data();
+          dataset[id] = data;
+        })
+      })
+
+      this.initDataset(dataset);
+      const initAnswer = ""; // Initialize the answer
+      this.selectAnswer(initAnswer, this.state.currentId);
+    })()
   }
 
   // Set the top of the scroll bar at the bottom of the scroll area to see the latest chat
---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAjt4Ksc2ZlC4xXjtn_MKU55MdoYu1fvcU",
+  authDomain: "chatbot-demo-e768a.firebaseapp.com",
+  projectId: "chatbot-demo-e768a",
+  storageBucket: "chatbot-demo-e768a.appspot.com",
+  messagingSenderId: "91874707825",
+  appId: "1:91874707825:web:8bb1559dfd7e108cf54ed0",
+  measurementId: "G-N33WT0VZKN"
+};
+
+export default firebaseConfig;
